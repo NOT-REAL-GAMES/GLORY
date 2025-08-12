bda12d994b3b4b7607f12f84e64427ab8a51bbc8
Árbjartur Kristjánsson <53384860+rbwldrf@users.noreply.github.com>
2025-08-12 03:16:54 +0000

    teehee :)

--- main.cpp
+++ main.cpp
@@ -402,7 +402,7 @@ private:
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.apiVersion = VK_API_VERSION_1_0;
+        appInfo.apiVersion = VK_API_VERSION_1_4;
         
         // Get required extensions from SDL
         uint32_t extensionCount = 0;
